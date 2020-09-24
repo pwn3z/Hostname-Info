5516a36c2045d7e5cc8e21f8393b169fee4c935e
Charlotte Wengi <71440287+mandr4x@users.noreply.github.com>
2020-09-24 01:34:29 -0400

    Update dr4x-info.sh

--- dr4x-info.sh
+++ dr4x-info.sh
@@ -2,14 +2,11 @@
 
 # Coded by Mandr4x #
 
-echo -e "Wordlist with hosts/ips: "; read host
+clear
+echo -e "Wordlist: "; read host
 
-for i in `cat $host`
-do
-request=$(curl -s -k https://check-host.net/ip-info?host=$i |grep -A 2 "Organization" |head -n2 |awk -F '<td>' '{print $2}' |awk -F '</td>' '{print $1}' |awk -F 'Organization' '{print $1}' |awk 'NF>0')
-request2=$(curl -s -k https://check-host.net/ip-info?host=$i |grep -A 3 "Country" |head -n4 |awk -F '<td>' '{print $1}' |awk 'NF>0' |awk -F '<strong>' '{print $2}' |awk 'NF>0' |awk -F '</strong>' '{print $1}')
-request3=$(curl -s -k https://check-host.net/ip-info?host=$i |grep -A 3 "Region" |head -n2 |awk -F '<td>' '{print $2}' |awk -F '</td>' '{print $1}' |awk -F 'Region' '{print $1}' |awk 'NF>0')
-request4=$(curl -s -k https://check-host.net/ip-info?host=$i |grep -A 3 "Host name" |head -n2 |awk -F '<td>' '{print $2}' |awk -F '</td>' '{print $1}' |awk -F 'Host name' '{print $1}' |awk 'NF>0')
-echo -e "\033[01;37m$i\033[01;37m\033[01;31m - \033[01;31m\033[01;33mProvider: $request\033[01;33m\033[01;31m | \033[01;31m\033[01;32mCountry: $request2\033[01;32m \033[01;31m|\033[01;31m \033[01;34mState:$request3\033[01;34m\033[01;31m |\033[01;31m \033[01;35mDNS: $request4\033[01;35m" 
-echo -e "$i - Provider: $request | Country: $request2 | State:$request3 | DNS: $request4" >> isp.txt
-done
+cat $host |parallel -j50 'a=$(curl -s -k https://check-host.net/ip-info?host={} |grep -A 2 "Organization" |head -n2 |cut -d ">" -f2 |cut -d "<" -f1 |xargs|sed "s/Organization//g")
+request2=$(curl -s -k https://check-host.net/ip-info?host={} |grep -A 3 "Country" |head -n4 |cut -d ">" -f2 |cut -d "<" -f1 |xargs|sed "s/Country //g")
+request3=$(curl -s -k https://check-host.net/ip-info?host={} |grep -A 3 "Host name" |head -n2 |cut -d ">" -f2 |cut -d "<" -f1 |xargs|sed "s/Host name //g")
+echo -e "\033[01;37m{}\033[01;37m\033[01;31m - \033[01;31m\033[01;33mProvider:$a\033[01;33m\033[01;31m | \033[01;31m\033[01;32mCountry: $request2\033[01;32m \033[01;31m|\033[01;31m \033[01;35mDNS: $request3\033[01;35m"'
+echo -e "{} - Provider:$a | Country: $request2 | DNS: $request3"
